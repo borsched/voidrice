679be6cfa763af21cafed4b8643e4a3ce339c898
borsched <75818517+borsched@users.noreply.github.com>
2024-05-16 12:57:25 -0500

    Update bar and monitors

--- .config/chadwm/scripts/bar.sh
+++ .config/chadwm/scripts/bar.sh
@@ -15,32 +15,9 @@ cpu() {
   printf "^c$green^ $cpu_val"
 }
 
-battery() {
-  printf "^b$black^  " # adds spaces in the front so the battery icon doesn't get covered
-  get_capacity="$(cat /sys/class/power_supply/BAT0/capacity)"
-
-	case "$(cat /sys/class/power_supply/BAT0/status 2>/dev/null)" in
-	Discharging) printf "^c$blue^   $get_capacity" ;;
-	Charging) printf "^c$blue^ 󱐋  $get_capacity" ;;
-	Full) printf "^c$blue^   $get_capacity" ;;
-  esac
-}
-
-brightness() {
-  printf "^c$red^   "
-  printf "^c$red^%.0f\n" $(cat /sys/class/backlight/*/brightness)
-}
-
 mem() {
   printf "^c$blue^^b$black^  "
   printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
-}
-
-wlan() {
-	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
-	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
-	esac
 }
 
 clock() {
@@ -55,5 +32,5 @@ while true; do
   [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ]
   interval=$((interval + 1))
 
-  sleep 1 && xsetroot -name "$(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
+  sleep 1 && xsetroot -name "$(cpu) $(mem) $(clock)"
 done
